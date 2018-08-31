d8b3470c45034951aac5970a80ac00dcb721b95e
Alabi Temitope <t.banji@rocketmail.com>
2018-08-31 22:28:40 +0100

    implementation in progress for additional features

---
+++ app/src/test/java/com/example/android/pets/ExampleUnitTest.java
@@ -0,0 +1,23 @@
+package com.example.android.pets;
+
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void subtraction_isCorrect() throws Exception {
+        assertEquals(0, 2 - 2);
+    }
+}
